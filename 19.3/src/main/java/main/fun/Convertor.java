a55024a24a3b4788b286a65086ffe2c84e6d7c0e
Elian Remaggi <54656152+ElianRemaggi@users.noreply.github.com>
2020-01-29 02:37:40 -0300

    Swing

---
+++ 19.3/src/main/java/main/fun/Convertor.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main.fun;
+
+/**
+ *
+ * @author elian
+ */
+public class Convertor {
+    
+         
+    
+         public static double calculateFarenheit(double temp) throws java.lang.NumberFormatException{
+        return ((temp*9/5) + 32);
+    }
+    
+}
